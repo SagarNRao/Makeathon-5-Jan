89715b7deddda4781597590f7c997583257d27f9
SagarNRao <sagarnrao154@example.com>
2024-01-05 17:57:40 +0530

    yeet

--- 3/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var animatedGradient = document.querySelector(".animated-gradient");
-
-  // Add a class to trigger the transition after a delay
-  setTimeout(function() {
-    animatedGradient.classList.add("smooth-transition");
-  }, 1000); // 1000 milliseconds (1 second) delay
-});
